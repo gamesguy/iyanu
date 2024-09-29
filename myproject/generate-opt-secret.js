b7ea61689abf03187489e82f9bf45e8f6db1b758
gamesguy <rereabidoye@gmail.com>
2024-09-29 16:42:15 +0100

    Revert "kk"
    This reverts commit 77bef66e67453ba13af151389bb9e802d70854ba.

--- myproject/generate-opt-secret.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a 32-byte random string and convert it to hex
-const otpSecret = crypto.randomBytes(32).toString('hex');
-
-console.log('Generated OTP Secret:', otpSecret);
